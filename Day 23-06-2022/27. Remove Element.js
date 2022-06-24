f52ed7712f0168e5abe002122deec37454b2281a
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-06-24 12:53:59 +0530

    27. remove element

--- Day 23-06-2022/27. Remove Element.js
+++ Day 23-06-2022/27. Remove Element.js
@@ -1,18 +1,16 @@
-const nums = [0,1,2,2,3,0,4,2];
-const val = 2;
-// const val = 3;
-// Output: 2, nums = [2,2,_,_]
+let arr = [0,1,2,2,3,0,4,2];
+let target = 2;
+let cnt = 0;
+let empty = [];
 
-const res = [];
-let count = 0;
-
-for (let i = 0 ; i < nums.length; i++){
-    if(nums[i] === val){
-        res.push('_');
-    } else {
-        count += 1;
-        res.push(nums[i]);
+for (let i=0 ; i<arr.length ; i++){
+    if (arr[i] === target){
+        empty.push('_');
+    }
+    else {
+        empty.push(arr[i]);
+        cnt += 1;
     }
 }
 
-console.log(res, count);
+console.log("Left element is = ",cnt,"\n Array is = ",empty);
